bcae405811aee477f8e68d23b0b6434a0d3dd0f9
yukinakai <10libra20.y@gmail.com>
2024-01-09 17:47:27 +0700

    add setSheetValues

---
+++ src/spreadsheet/setSheetValues.ts
@@ -0,0 +1,20 @@
+function setSheetValues(
+  worksheet: GoogleAppsScript.Spreadsheet.Sheet,
+  setData: any[][],
+  row: number,
+  column: number,
+  numRows: number,
+  numColumns: number
+) {
+  let retries = 0;
+  while (retries < 3) {
+    try {
+      worksheet.getRange(row, column, numRows, numColumns).setValues(setData);
+    } catch (e) {
+      retries++;
+      if (retries === 3) throw e;
+      console.warn(`エラーが発生しました。リトライします。${retries}回目`);
+      Utilities.sleep(1000);
+    }
+  }
+}
